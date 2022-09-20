eb0bdd094ff5381bf6805826d69645c6ff0a5766
Enes Orhan <enesorhan_1366@hotmail.com>
2022-09-20 15:01:36 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList primeNumber = new ArrayList();
+            ArrayList notPrimeNumber = new ArrayList();
+            int kontrol = 0;
+            int asalToplam= 0;
+            int asalOlmayanToplam = 0;
+
+            Console.WriteLine("20 adet dogal sayi giriniz: ");
+
+            for (int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if(sayi>=0){
+                    if(sayi==0 || sayi ==1){
+                        notPrimeNumber.Add(sayi);
+                    }
+                    else{
+                        for (int j = 2; j < sayi; j++)
+                        {
+                            if (sayi % j == 0)
+                                kontrol++;
+                        }
+                        if(kontrol == 0)
+                            primeNumber.Add(sayi);
+                        else
+                            notPrimeNumber.Add(sayi);
+                        kontrol=0;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hatali Sayi Girdiniz...");
+                }
+            }
+            primeNumber.Sort();
+            notPrimeNumber.Sort();
+
+            Console.WriteLine("***** Asal Sayilar *****");
+            foreach (var sayi in primeNumber){
+                Console.WriteLine(sayi);
+                asalToplam = asalToplam + Convert.ToInt32(sayi);
+            }
+
+            Console.WriteLine("***** Asal Olmayan Sayilar *****");
+            foreach (var sayi in notPrimeNumber){
+                Console.WriteLine(sayi);
+                asalOlmayanToplam = asalOlmayanToplam + Convert.ToInt32(sayi);
+            }
+            
+            Console.WriteLine("Asal Sayi Listesinin Eleman Sayisi: " + primeNumber.Count);
+            Console.WriteLine("Asal Sayilarin Ortalamasi: " + (float)asalToplam/primeNumber.Count);
+            Console.WriteLine("Asal Olmayan Sayi Listesinin Eleman Sayisi: " + notPrimeNumber.Count);
+            Console.WriteLine("Asal Olmayan Sayilarin Ortalamasi: " + (float)asalOlmayanToplam/notPrimeNumber.Count);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Patika Homework 1 - Algorithm Questions About Collections
+
+## 1. Work
+Throw the 20 positive numbers entered from the keyboard into 2 separate lists as prime and non-prime. (Write using the ArrayList class.)
+
+- Block negative and non-numeric entries.
+- Print the elements of each array, from largest to smallest.
+- Print the number of elements and the average of both arrays to the screen.
+
+## 2. Work
+Write a program that finds the largest 3 and the smallest 3 of the 20 numbers entered from the keyboard, takes the averages of both groups and prints these averages and average totals to the console. (Write using the Array class.)
+
+## 3. Work
+Write a program that stores the vowels in a sentence entered from the keyboard in an array and sorts the elements of the array.
+
